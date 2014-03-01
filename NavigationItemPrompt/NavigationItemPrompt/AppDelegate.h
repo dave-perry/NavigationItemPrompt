274fd203f9cf79787cca42cadd3345bf1fd2fb2f
Dave Perry <insertwittyname@gmail.com>
2014-03-01 14:35:47 +0000

    Demonstrate problem on iOS7.

---
+++ NavigationItemPrompt/NavigationItemPrompt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NavigationItemPrompt
+//
+//  Created by David Perry on 01/03/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NavigationItemPrompt/NavigationItemPrompt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NavigationItemPrompt
+//
+//  Created by David Perry on 01/03/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
